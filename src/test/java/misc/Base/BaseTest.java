45d5553972956beeed1ee9223eb10fdc6be1a98f
Marko Petrovic <marko.petrovic@seavus.com>
2019-10-01 08:07:49 +0200

    test

--- src/test/java/misc/Base/BaseTest.java
+++ src/test/java/misc/Base/BaseTest.java
@@ -2,8 +2,6 @@ package misc.Base;
 
 import misc.BrowserFactory.BrowserFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
